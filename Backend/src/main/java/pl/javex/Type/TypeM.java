3c16b112ca8d3e4b698809e8123c4b17d1cd7c68
Radosław Boczoń <88336041+ManiekMelancholik@users.noreply.github.com>
2023-03-28 10:32:23 +0200

    Feature product desc&type models

---
+++ Backend/src/main/java/pl/javex/Type/TypeM.java
@@ -0,0 +1,32 @@
+package pl.javex.Type;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import pl.javex.Product.ProductM;
+import pl.javex.ProductType.ProductTypeM;
+
+@Entity
+@Table( name = "types" )
+public class TypeM {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected long Id;
+	
+	protected String name;
+	
+	@ManyToOne
+	@JoinColumn(name="id_product_type")
+	protected ProductTypeM productType;
+	
+	@OneToMany(mappedBy="type")
+	protected List<ProductM> products;
+}
