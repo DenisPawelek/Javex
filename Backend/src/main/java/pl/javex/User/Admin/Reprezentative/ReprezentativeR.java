2e4450673e0fdfc0ced6b22385944515c339eacf
Radosław Boczoń <88336041+ManiekMelancholik@users.noreply.github.com>
2023-03-28 11:35:17 +0200

    Feature user usuwanie niepotrzebnych

--- Backend/src/main/java/pl/javex/User/Admin/Reprezentative/ReprezentativeR.java
+++
@@ -1,9 +0,0 @@
-package pl.javex.User.Admin.Reprezentative;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReprezentativeR extends JpaRepository<ReprezentativeM, Long> {
-
-}
