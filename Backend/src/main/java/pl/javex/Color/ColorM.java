56ac439bf80246dcfe4140994f2820ae81792399
Radosław Boczoń <88336041+ManiekMelancholik@users.noreply.github.com>
2023-03-28 10:43:07 +0200

    Feature product color&category models

---
+++ Backend/src/main/java/pl/javex/Color/ColorM.java
@@ -0,0 +1,29 @@
+package pl.javex.Color;
+
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import pl.javex.Product.ProductM;
+import pl.opole.uni.springWebApp.models.Car;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
+@Table(name = "colors")
+public class ColorM {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	protected long Id;
+	
+	protected String name;
+	
+	@OneToMany(mappedBy="color")
+	protected List<ProductM> products;
+}
