826d666eb6967eeec5af3946185658953a3631bd
DPAW <denispawelek1307@gmail.com>
2023-04-03 11:06:36 +0200

    Merge branch 'Develop' into FeatureFront/StartPage

--- Backend/src/main/java/pl/javex/Contract/ContractR.java
+++
@@ -1,9 +0,0 @@
-package pl.javex.Contract;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContractR extends JpaRepository<ContractM, Long>{
-
-}
--- Backend/src/main/java/pl/javex/User/Client/ClientR.java
+++
@@ -1,9 +0,0 @@
-package pl.javex.User.Client;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientR extends JpaRepository<ClientM, Long> {
-
-}
